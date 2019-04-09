da74b577737da8540dab348d9df7a125463a56cc
WGussev <wsewolodgusew@yandex.ru>
2019-04-09 14:57:34 +0300

    Add imgCutter

---
+++ MOS_SK_GK4_250/readme.txt
@@ -0,0 +1,14 @@
+Pøi jakémkoliv užití tìchto dat je nutné uvádìt copyright ve znìní “Data poskytl a zpracoval © GISAT (2007)”. 
+Tato data nesmí být používána ke komerèním úèelùm, v pøípadì zájmu o komerèní využití nás kontaktujte.
+
+It is obligatory to present the copyright notice “Data provided and processed by © GISAT (2007)” whenever the data are used.
+These data cannot be used for commercial purposes. If any interest in commercial use, please, contact us.
+
+GISAT s.r.o. 
+Charkovská 7 
+101 00 Praha 10 
+Czech Republic
+
+Tel: +420 271741935 
+Fax: +420 271741936
+E-mail: gisat@gisat.cz
---
+++ img_cutter.py
@@ -0,0 +1,40 @@
+from imageio import imread, imwrite
+import matplotlib.pyplot as plt
+import numpy
+
+
+class imgCutter:
+    """ """
+
+    def __init__(self, path):
+        self.img = imread(path)
+
+    def cut_image(self, width=200, height=200):
+        self.rows = self.img.shape[0] // height + 1
+        self.cols = self.img.shape[1] // width + 1
+        self.parts = [[0 for i in range(self.cols)] for j in range(self.rows)]
+        print(len(self.parts))
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.parts[i][j] = self.img[i *200:(i+1)*200, j*200:(j+1)*200, :]
+        print(len(self.parts))
+
+    def show_image(self):
+        plt.imshow(self.img)
+        plt.show()
+
+    def show_parts(self):
+        fig = plt.figure(figsize=(self.rows, self.cols))
+        for i in range(self.rows):
+            for j in range(self.cols):
+                num = i * self.cols + j + 1
+                img = self.parts[i][j]
+                fig.add_subplot(self.rows, self.cols, num)
+                plt.imshow(img)
+        plt.show()
+
+
+im = imgCutter(
+    '/home/lodya/Documents/DigDes/sat-img-lab/MOS_SK_GK4_250/MOS_SK_GK4_250.tif')
+im.cut_image()
+im.show_parts()
